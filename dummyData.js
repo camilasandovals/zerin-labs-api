a343a051d037deef2b3b6ccdec9073cf2e2d9c35
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-06 12:48:28 -0400

    added dummyData

---
+++ dummyData.js
@@ -0,0 +1,20 @@
+const userDummyData = [
+    {
+        username: "user1",
+        password: "12345678"
+    },
+    {
+        username: "user2",
+        password: "12345678"
+    },
+]
+
+const medicationDummyData = [
+    {
+        name: "ibuprofen",
+        dosage: 200,
+        quantity: 1,
+        amount: "mg",
+        frequency: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    }
+]
