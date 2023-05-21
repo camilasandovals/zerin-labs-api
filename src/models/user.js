7c630b494ad882386c854125eaea1b456071cb3e
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-21 16:22:29 -0400

    changed default user logo

--- src/models/user.js
+++ src/models/user.js
@@ -34,7 +34,7 @@ const userSchema = new Schema({
         type: String,
     },
     img: {
-        type: String, default: "https://cdn-icons-png.flaticon.com/512/1053/1053244.png"
+        type: String, default: "https://milasnewbucket.s3.us-east-2.amazonaws.com/user+icon+Zerinlabs2.png"
     },
 })
 
