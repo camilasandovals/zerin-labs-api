aa350f4331d9d59e0c42994bc34eda0fedf627d4
Camila Sandoval <83136741+camilasandovals@users.noreply.github.com>
2025-01-19 14:24:59 -0500

    Finished

--- env.js
+++
@@ -1,5 +0,0 @@
-export const MONGOURI =
-  "mongodb+srv://mariacamilasandoval:SBI9uRWjnPXIAbG8@zerinlabs.s4fehwm.mongodb.net/";
-export const secretKey =
-  "q34o8ryfi3u4fyc9384ejvheriugvyei756583934885yrhufuhiegwi22005-45046u0946uoigthtesfjhjf";
-export const salt = "$2b$10$9VtRvhIsMZT0X6SFxVtYbu";
